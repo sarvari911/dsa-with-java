4f2d0cb6e2df6333d57c472947e84756fd02b18a
sarvari911 <sarvarikhatoon44@gmail.com>
2025-08-09 21:32:38 +0530

    Create HollowRhombus.java

---
+++ patterns/HollowRhombus.java
@@ -0,0 +1,21 @@
+public class HollowRhombus {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+            // spaces
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print("  ");
+            }
+            // stars & spaces
+            for (int j = 1; j <= rows; j++) {
+                if (i == 1 || i == rows || j == 1 || j == rows) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
