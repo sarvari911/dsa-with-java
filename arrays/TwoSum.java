292bcba22d5051f2780a655f0307896547d01d01
sarvari911 <sarvarikhatoon44@gmail.com>
2025-08-09 13:49:01 +0530

    "Added Two Sum problem and solution"

---
+++ arrays/TwoSum.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        HashMap< Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            int complement = target - a
+        }
+        return ans;
+    }
+}
